1ad38c6be42c86eb6c6b8e044f3bc59829ac3f32
ZadenMubarak <zaden.ngobeni@gmail.com>
2022-09-22 14:15:07 -0700

    psuedo -.exp added

---
+++ engine/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './packs/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div class="bg">
+        <ul class="glass">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ engine/src/packs/Navbar.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {FaBars, FaTimes} from 'react-icons/fa'
+import './Navbar.css';
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = ()=>setClick(click)
+
+    return(
+        <div className="header">
+            <div className="container">
+                <h1>web3 <span className="primary">Drive</span></h1>
+                <ul className={click ? 'nav-menu active' : "nav-menu"}>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="/">Features</a>
+                    </li>
+
+                    <li>
+                        <a href="/">Earn</a>
+                    </li>
+
+                    <li>
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+                <div className="btn-group">
+                    <button className="btn">connect wallet</button>
+                </div>
+                <div className="hamburger" onClick={handleClick}>
+
+                    {click ? (<FaTimes size={20} style={{color: '#333'}}/>) : (<FaBars size={20} style={{color: '#333'}} />)}
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar;
